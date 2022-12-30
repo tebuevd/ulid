8409aaec46a28a81f5e84cc755871d10d9911736
dinislam tebuev <3241533+tebuevd@users.noreply.github.com>
2022-12-29 20:47:45 -0800

    feat: 0.1.1-alpha.2.0 release

--- packages/browser/esm/index.js
+++
@@ -1,2 +0,0 @@
-function e(){if(typeof window<"u"&&window instanceof Window)return window.crypto.getRandomValues;if(typeof self<"u")return self.crypto.getRandomValues;if(typeof crypto<"u")return crypto.getRandomValues}export{e as getPrng};
-//# sourceMappingURL=index.js.map
